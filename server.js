c9fb2341c54ae6e288a2d8612417812ea7bdc3b9
sriman9859cd <sriman.parthasarathy@crystaldelta.com>
2026-05-05 19:25:11 +0530

    import dotenv file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 
 import express from "express"
+import dotenv from "dotenv"
 
 let PORT = 3000
 
 const app = express();
-app.listen (PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server Started on port number ${PORT}`)
 })
